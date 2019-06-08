85cb9c6686b3cb1b76ceca9be24127945ccc90fc
Vijayakumar Shanmugam <vijay@Vijayakumars-MacBook-Air.local>
2019-06-08 20:01:37 +0530

    Updated SchemaOrg

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,19 @@
+const schemaorg = require('../SchemaScript.json');
 module.exports = {
-    title: 'Study Blazor',
+    title: 'Study Blazor .NET',
     description: 'Blazor tutorial for beginners, in simple and easy steps starting from basics to advanced concepts with more code snippets.',
     head: [
         ['link', { rel: 'icon', href: `/logo.png` }],
+        ['link', { rel: 'shortcut icon', href: `/favicon.ico`, type:'image/x-icon' }],
         ['meta', { name: 'robots', content: 'all' }],
         ['meta', { name: 'keywords', content: 'Blazor Tutorial, SPA, ASP.Net Core, C#, Javascript Alternative, Web Assembly' }],
+        ['meta', { property:'og:type',content:'website'}],
+        ['meta', { property:'og:title',content:'Study Blazor'}],
+        ['meta', { property:'og:description',content:'Blazor tutorial for beginners, in simple and easy steps starting from basics to advanced concepts with more code snippets.'}],      
+        ['meta', { property:'og:locale',content:'en_US'}],
+        ['meta', { property:'og:url',content:'https://studyblazor.com'}],
+        ['meta', { property:'og:image',content:'https://studyblazor.com/logo.png'}],
+        ['script',{type:'application/ld+json'},JSON.stringify(schemaorg)],
         ['script', { async:'' , src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' }],
         ['script', {},
             '(adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-9955716341281227", enable_page_level_ads: true });'],
