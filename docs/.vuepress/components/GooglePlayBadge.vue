9a6463fe4d9cd78cc635210042c5f55af200baff
Vijayakumar Shanmugam <vijay@Vijayakumars-MacBook-Air.local>
2019-06-29 16:42:30 +0530

    Updated Hyperlink for GooglePlay Badge

--- docs/.vuepress/components/GooglePlayBadge.vue
+++ docs/.vuepress/components/GooglePlayBadge.vue
@@ -1,6 +1,8 @@
 <template>
   <div align="center">
-    <h4>Download StudyBlazor App</h4>
+    <a href='https://play.google.com/store/apps/details?id=com.studyblazor.andriod' target='_blank'>
+      <h4>Download StudyBlazor App</h4>
+    </a>
     <a href='https://play.google.com/store/apps/details?id=com.studyblazor.andriod' target='_blank'>
         <img alt='Get it on Google Play' src='/google-play-badge.png'/>
     </a>
